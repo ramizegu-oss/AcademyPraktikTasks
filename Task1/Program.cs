5e3f42433d32aa960f1cb7e21101a1c033adddfb
ramizegu-oss <ramizegu@code.edu.az>
2026-05-21 21:43:50 +0400

    first file

---
+++ Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿using ClassInTheAcademy.Task1;
+
+UtilityService service1 = new();
+
+
+//TASKS WITH STRING:
+
+//Console.WriteLine("String Input:");
+//service1.word = Console.ReadLine();
+
+//Console.WriteLine(service1.VowelsLettersCount());
+
+//Console.WriteLine(service1.StrReverse());
+
+//Console.WriteLine(service1.Palindrome());
+
+//Console.WriteLine(service1.CheckBigLetter());
+
+//Console.WriteLine(service1.Equals()); 
+
+
+//tasks with ARRAY:
+
+service1.arrInt = [1,2, 8, 6, 4, 3, 88, 4, 45];
+
+//Console.WriteLine(service1.BiggestElem());
+
+
+//service1.CutElem();
+
+
+//Console.WriteLine(service1.CountLes()); 
+
+
+//Console.WriteLine(service1.MediumValue()); 
+
+//service1.Unick();
+
+
+
+
+
---
+++ Task1/UtilityService.cs
@@ -0,0 +1,167 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ClassInTheAcademy.Task1
+{
+    internal class UtilityService
+    {
+        public string word;
+        public int[] arrInt;
+        public int VowelsLettersCount()
+        {
+            char[] volwesletters = ['a','e','u','i','o','y'];
+            int count = 0;
+
+
+            foreach (char item in word.ToLower())
+            {
+                if (volwesletters.Contains(item))
+                {
+                    count++;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return count;
+            
+        }
+
+
+
+        public string StrReverse()
+        {
+            char[] arrWord = word.ToArray();
+
+            var res = arrWord.Reverse();
+
+
+            return string.Concat(res);
+        }
+
+
+
+        public int MediumValue()
+        {
+            int sum = 0;
+
+            foreach (var item in arrInt)
+            {
+                sum += item;
+            }
+           return sum / arrInt.Length;
+        }
+
+
+        public int BiggestElem()
+        {
+            int BiggestValue = arrInt[0];
+            for (int i = 1; i < arrInt.Length; i++)
+            {
+                if (arrInt[i] > BiggestValue)
+                {
+                    BiggestValue = arrInt[i];
+                    
+                }
+
+            }
+            return BiggestValue;
+        }
+
+
+        public void CutElem()
+        {
+            foreach (var item in arrInt)
+            {
+                if (item % 2 == 0)
+                {
+                    Console.WriteLine(item);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            
+        }
+
+
+
+        public bool Palindrome()
+        {
+
+            char[] wordArr = word.ToCharArray();
+            Array.Reverse(wordArr);
+
+
+
+            if (word == string.Concat(wordArr))
+            {
+                word.ToString();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public int CheckBigLetter()
+        {
+            int count = 0;
+
+            int res = word.Count(m =>  char.IsUpper(m));
+
+            return res;
+           
+            
+        }
+
+
+
+        public int CountLes()
+        {
+
+            int count = 0;
+            foreach (var item in arrInt)
+            {
+                if (item < 0)
+                {
+                    count++;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return count;
+        }
+
+
+        public bool Equals()
+        {
+            Console.WriteLine("Input second text");
+            string word2 = Console.ReadLine();
+            if (word.ToLower() == word2.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public void Unick()
+        {
+            
+            
+
+
+            
+
+        }
+    }
+}
